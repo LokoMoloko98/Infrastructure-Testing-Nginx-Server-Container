5330b212ed5e8b59cd4407be2446de39401ae644
Moloko Mokubedi <molavovic@gmail.com>
2023-05-05 11:27:57 +0200

    insertip 1.1

--- insertip.sh
+++ insertip.sh
@@ -1,4 +1,6 @@
-ex index.html <<eof
+#!/bin/bash
+
+ex ./index.html <<eof
 58 insert
                 <p class="p404" data-depth="0.50">Hey There! <br> The container is successfully deployed! at host $(hostname -f)</p>
                 <p class="p404" data-depth="0.50">Hey There! <br> The container is successfully deployed! at host $(hostname -f)</p>
