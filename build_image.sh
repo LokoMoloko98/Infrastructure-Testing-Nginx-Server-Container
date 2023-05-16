0b661401bb5a016fcba69439678a37b92264d384
Moloko Mokubedi <communication@moloko-mokubedi.co.za>
2023-05-16 17:10:55 +0200

    Update build_image.sh to user docker as the user
    No sudo in the ec2!!!!

--- build_image.sh
+++ build_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the docker build command
-sudo docker build \
+docker build \
 --build-arg PERSONAL_ACCESS_TOKEN="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" \
 --build-arg GITHUB_USERNAME="LokoMoloko98" \
 --build-arg REPOSITORY_NAME="Infrastructure-Testing-Nginx-Server-Container" \
